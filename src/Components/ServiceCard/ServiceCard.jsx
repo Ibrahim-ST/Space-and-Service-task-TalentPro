f5b745bb628455dc3fc25e92b965612357a020c7
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-07-05 18:15:01 +0600

    fixed an error

--- src/Components/ServiceCard/ServiceCard.jsx
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -5,14 +5,14 @@ import { FaArrowRight } from "react-icons/fa";
 
 const ServiceCard = ({service}) => {
     const {image, title, description} = service
-    console.log(service);
+    
     return (
         <div className='w-48 h-64 relative my-4'>
             <img className='h-20' src={image} alt="" />
             <h4 className='font-bold mt-5 w-2/3'>{title}</h4>
             <p className='my-4'>{description}</p>
             <div className=' absolute bottom-0'>
-            <button className='flex items-center gap-2 font-bold text-[#32C5FF]'><span>Learn more</span><FaArrowRight></FaArrowRight> </button>
+            <button className='flex items-center gap-2 font-bold text-[#32C5FF]'><span>Learn More</span><FaArrowRight></FaArrowRight> </button>
             </div>
         </div>
     );
