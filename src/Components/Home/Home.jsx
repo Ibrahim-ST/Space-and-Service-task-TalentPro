c723c82d55e74661e18823852f02eae38b00078b
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-07-05 17:58:59 +0600

    fetched services data from server and displayed in UI, used vercel to handle server data and deployed to netlify for live preview

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"; 
+import ServiceCard from "../ServiceCard/ServiceCard";
+
+const Home = () => {
+    const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("https://talent-pro-task-server.vercel.app/data")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  console.log(services);
+    return (
+        <>
+      <div className="max-w-5xl mx-auto my-10">
+        <section className="text-center">
+          <h3 className="text-3xl font-bold font-mono">
+            Explore Our Storage Space, Co-Working Space, <br /> Co-Warehouse &
+            Services
+          </h3>
+          <p className="text-gray-500 my-6 font-serif">
+            Carefully crafted for a hassle-free and affordable experience
+          </p>
+        </section>
+
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 justify-items-center">
+          {services.map((service) => (
+            <ServiceCard
+            key={service.id}
+            service={service}></ServiceCard>
+          ))}
+        </section>
+      </div>
+    </>
+    );
+};
+
+export default Home;
---
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { FaArrowRight } from "react-icons/fa";
+
+const ServiceCard = ({service}) => {
+    const {image, title, description} = service
+    console.log(service);
+    return (
+        <div className='w-48 h-64 relative my-4'>
+            <img className='h-20' src={image} alt="" />
+            <h4 className='font-bold mt-5 w-2/3'>{title}</h4>
+            <p className='my-4'>{description}</p>
+            <div className=' absolute bottom-0'>
+            <button className='flex items-center gap-2 font-bold text-[#32C5FF]'><span>Learn more</span><FaArrowRight></FaArrowRight> </button>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
